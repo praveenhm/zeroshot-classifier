65570807bbfe0a853c65ce56310fa3676fc5f5a6
praveenhm <praveenhm2@yahoo.com>
2024-01-30 20:32:46 -0800

    Add config.py file with Hugging Face API token

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+# Hugging Face Access Token
+# Replace 'your_hugging_face_api_token_here' with your actual Hugging Face API token.
+HF_ACCESS_TOKEN = "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"

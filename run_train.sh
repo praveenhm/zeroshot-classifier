6f55026abb5e89c7bfd2d9f523d82b67b7aabeb5
praveenhm <praveenhm2@yahoo.com>
2024-01-30 21:25:28 -0800

    Add run_train.sh script to execute Python script with specified datasets

---
+++ run_train.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run the Python script with the specified datasets
+python train_eval_acuvity.py -ds_heldout "wellformedquery, financialphrasebank, rottentomatoes, amazonpolarity, imdb, appreviews,  wikitoxic_toxicaggregated,wikitoxic_obscene, wikitoxic_threat, wikitoxic_insult, wikitoxic_identityhate, hateoffensive, hatexplain,trueteacher, spam, massive, banking77, emotiondair, emocontext, empathetic, agnews, yahootopics,biasframes_offensive, biasframes_sex, biasframes_intent"

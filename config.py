4c0bd3839369b0f6f3f6934052bd571878a408a3
praveenhm <praveenhm2@yahoo.com>
2024-01-31 21:48:17 -0800

    Update Hugging Face API token

--- config.py
+++ config.py
@@ -2,5 +2,6 @@
 
 # Hugging Face Access Token
 # Replace 'your_hugging_face_api_token_here' with your actual Hugging Face API token.
-HF_ACCESS_TOKEN = "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"
+#HF_ACCESS_TOKEN = "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"
+HF_ACCESS_TOKEN = "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" # write
 WANDB_ACCESS_TOKEN = "1a4f41eb20e41af525cc850148aea6413bfb6a1e"
